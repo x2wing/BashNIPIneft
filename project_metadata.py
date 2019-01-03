ece09371ae2d44def5d9bab039de4e9f1d4aef7b
DESKTOP-P8NVTRV\magistr <x2wing@gmail.com>
2019-01-03 22:00:42 +0500

    comments added

--- project_metadata.py
+++ project_metadata.py
@@ -6,18 +6,25 @@ import time
 
 
 class Metadata():
-    cur_filename = ""
-    cur_dataset = ""
+    """ Класс работы с метаданными проекта"""
 
-    current_datafile_path = ""
-    current_project_dir = ""
+    cur_filename = ""  # имя текущего hdf5-файла(без пути)
+    cur_dataset = ""  # имя текущего датасета
+
+    # current_datafile_path = "" #
+
+    current_project_dir = ""  # путь к папке текущего проекта
+    # метаданные текущего выбранного(в QTreeView) датасета
     current_dataset_metadata = defaultdict(dict)
 
     def __init__(self, metafile_path: str):
         data = {"version": 0.1}
+        # если файл метаданных проекта пустой
         if not self._get_metadata(metafile_path):
+            # запишем в него версию
             self._rw_yaml_config(metafile_path, data=data, mode='w')
         self.metafile_path = metafile_path
+        # сохраним путь к папке текущего проекта
         Metadata.current_project_dir = os.path.dirname(self.metafile_path)
 
     def _rw_yaml_config(self, file_path: str, data=None, mode='r'):
@@ -32,54 +39,74 @@ class Metadata():
                 yaml.dump(data, stream, default_flow_style=False, allow_unicode=True)
 
     def _get_metadata(self, metafile_path):
+        """ если данные корректные метод возвращает словарь метаданных
+        в противном случае возвращает None"""
         try:
             return self._rw_yaml_config(metafile_path, mode='r')
         except:
             return None
 
     def get_metadata(self):
+        """ public метод получения метаданных"""
         return self._get_metadata(self.metafile_path)
 
     def save_metadata(self, new_metadata: dict):
+        """Добавление\обновление метаданных в файл метаданных"""
+        # получаем все метаданные из файла
         metadata_from_file = self._get_metadata(self.metafile_path)
-        # print('metadata_from_file', metadata_from_file)
+        # если метаданные получены
         if metadata_from_file:
+            # создаем пустой словарь
             metadata = OrderedDict({})
+            # и копируем туда данные
             metadata.update(metadata_from_file)
-            # вытаскиваем ключ из добавляемого словаря
+            # вытаскиваем ключ(один) из добавляемого словаря
             new_key = next(iter(new_metadata.keys()))
+            # если такой ключ существует в существующем словаре
             if new_key in metadata:
+                # перебираем данные(ключ и значение) второго уровня
                 for key, value in new_metadata[new_key].items():
                     # metadata[new_key].setdefault(key, value)
+                    # добавляем новые данные под старый ключ
                     metadata[new_key].update(new_metadata[new_key])
             else:
+                # если такого ключа не существует просто добавляем весь словарь
                 metadata.update(new_metadata)
-
+            # записывем измененный словарь в метафайл проекта
             self._rw_yaml_config(self.metafile_path, metadata, mode='w')
         else:
+            # если метаданные не получны выбросим исключение
             assert False, f"файл {self.metafile_path} не прочитан, пуст или не существует"
 
     def set_metadata(self, n_data: np.ndarray):
+        """Метод создания текущего элемента метаданных """
+        # словарь с метаданными
         metadata = OrderedDict({
+            # текущее имя файла
             Metadata.cur_filename:
                 {
+                    # время создания файла
                     'file create at': time.time(),
+                    # имя текущего датасета
                     Metadata.cur_dataset: {
-                        'dset create at': time.time(),
-                        'shape': n_data.shape,
-                        'min': n_data.min(),
-                        'max': n_data.max(),
+
+                        'dset create at': time.time(), #время создания датасета
+                        'shape': n_data.shape, # размерность датасета
+                        'min': n_data.min(), # минимальное значение датасета
+                        'max': n_data.max(), # максимальное значение датасета
                     }
                 }
         })
-
+        # сохраняем метаданные
         self.save_metadata(metadata)
         # обновляем метаданные текущего элемента QTreeView
         Metadata.current_dataset_metadata = metadata
 
+    # статический метод получения метаданных текущего датасета
     @staticmethod
-    def get_current_dataset_metadata(filename,dataset):
+    def get_current_dataset_metadata(filename, dataset):
         return Metadata.current_dataset_metadata[filename][dataset]
+
 
 if __name__ == '__main__':
     Meta = Metadata(r"D:\projects\BashNIPIneft-dev\HDF_FILES\geosim.meta")
